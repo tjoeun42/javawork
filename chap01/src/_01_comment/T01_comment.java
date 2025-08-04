9d976263ff9b26a4541644b9da693645d26e4f28
tjoeun42 <tjoeunit@gmail.com>
2025-08-04 17:22:45 +0900

    chap01_2교시

---
+++ chap01/src/_01_comment/T01_comment.java
@@ -0,0 +1,20 @@
+package _01_comment;
+
+public class T01_comment {
+
+	public static void main(String[] args) {
+		// 한줄 주석
+		//  - 주석은 컴파일하지 않음
+		//  - 실행되지 않는다
+		//  - 설명, 날짜, 누가 만들었는지, 중요한 프로그램 설명
+		//  - 단축키 : ctrl + /  (토글키)
+		
+		/*
+		 여러줄 주석
+		   - 이 안의 모든 글씨는 주석임
+		   - 단축키 : ctrl + shift + /
+		   - 해제단축키 : ctrl + shift + \  
+		 */
+	}
+
+}
