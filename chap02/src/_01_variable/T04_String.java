eef7008c8497d00f878eb0276aee682a60764799
tjoeun42 <tjoeunit@gmail.com>
2025-08-04 20:48:50 +0900

    1일차_6교시_scanner

--- chap02/src/_01_variable/T04_String.java
+++ chap02/src/_01_variable/T04_String.java
@@ -5,7 +5,26 @@ public class T04_String {
 	public static void main(String[] args) {
 		String str1 = "801호";
 		String str2 = "java 풀스택";
-		System.out.println(str1 + str2);
+		System.out.println(str1 + " " + str2);
+		
+		// escape sequence(이스케이프 문자)
+		/*
+		 \ + 문자 : 조합으로 특수 기능 수행
+		 
+		 \n : 줄바꿈
+		 \t : 탭만큼 띄우기
+		 \" : 쌍따옴표 출력
+		 \\ : 역슬래스(\) 출력
+		 */
+		
+		System.out.println("강의실 : 801호\n강좌명 : \"java\t풀스택\"");
+
+		/*
+		 강의실 : "801호
+		 강좌명 : "java 풀스택 웹&앱 개발자"
+		 
+		 강의실 : "801호 \ 강좌명 : "java 풀스택 웹&앱 개발자"
+		 */
 
 	}
 
---
+++ chap02/src/_02_Scanner/T02_Ex01.java
@@ -0,0 +1,10 @@
+package _02_Scanner;
+
+public class T02_Ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
