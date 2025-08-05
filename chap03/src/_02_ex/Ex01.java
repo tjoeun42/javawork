4b08a01d398be358754f30393d80214143607a78
tjoeun42 <tjoeunit@gmail.com>
2025-08-05 21:21:26 +0900

    2일차_6교시_연습문제

---
+++ chap03/src/_02_ex/Ex01.java
@@ -0,0 +1,92 @@
+package _02_ex;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		// 1. 사용자로부터 정수를 입력받아 양수인지 양수가 아닌지 판별하는 결과 출력
+		System.out.print("정수 입력 : ");
+		int num1 = scan.nextInt();
+		
+		String result = num1 > 0 ? "양수" : "양수가 아니다";
+		System.out.println(result);
+		
+		System.out.println(num1 > 0 ? "양수" : "양수가 아니다");
+		
+		// 2. 사용자로부터 정수를 입력받아 양수, 0, 음수 판별하는 결과 출력
+		System.out.print("정수 입력 : ");
+		int num2 = scan.nextInt();
+		
+		System.out.println(num2 > 0 ? "양수" : num2 == 0 ? "0" : "음수");
+		
+		// 3. 사용자로부터 정수를 입력받아 짝수인지 홀수인지 판별하는 결과 출력
+		System.out.print("정수 입력 : ");
+		int num3 = scan.nextInt();
+		
+		System.out.println(num3 % 2 == 0 ? "짝수" : "홀수");
+		
+		/*
+		4. 각 인원수에 따라 사탕을 동일하게 나눠 주려고 한다
+		   사용자로부터 인원수와 사탕의 갯수를 입력받아
+		   1인당 동일하게 나눠가진 사탕 개수와
+		   나눠주고 남은 사탕 갯수를 출력하기
+		*/
+		System.out.print("인원수 입력 : ");
+		int person = scan.nextInt();
+		System.out.print("사탕 갯수 입력 : ");
+		int candy = scan.nextInt();
+		
+		int share = candy / person;
+		int remainder = candy % person;
+		System.out.println("1인당 사탕 갯수 : " + share);
+		System.out.println("남은 사탕 갯수 : " + remainder);
+
+		/*
+		5. 키보드로 입력 받은 값들을 변수에 저장하고 저장된 변수의 값을 다음과 같이 출력하기
+		   이 때, 성별이 'M'이면 남학생, 'M'이 아니면 여학생으로 출력.
+				
+			ex.
+			이름 : 이준기
+			학년(숫자만) : 2
+			반(숫자만) : 7
+			번호(숫자만) : 3
+			성별(M/F) : M
+			성적(소수점 둘째 짜리까지) : 97.35
+				
+			2학년 7반 3번 이준기 남학생의 성적은 97.35이다.
+		 */
+		
+		/*
+		6. 사용자로부터 나이를 입력받아 13세이하이면 어린이
+		    14세~19세 이하이면 청소년
+		    20세 이상이면 성인으로 출력
+		 */
+		
+		/*
+		7. 사용자로부터 국어, 영어, 수학 점수를 입력받아 
+		   세 과목 점수가 각각 40점 이상이면서 평균이 60점 이상일 때 합격, 아니라면 불합격을 출력.
+		 */
+		
+		
+		/*8. 주민번호를 입력받아 남자인지 여자인지 출력하기
+				
+			ex.
+			주민번호를 입력하세요(- 포함) : 123456-3123456
+		 */
+		
+		/*
+		9.  사용자로부터 두 개를 입력 받아 num1, num2에 저장하고
+		    또 다른 정수를 입력 받아 그 수가 num1 미만거나 num2 초과이면 true를 출력하고
+		    아니면 false를 출력. (단, num1은 num2보다 작아야 함)
+		 */
+		
+		/*
+		10. 사용자로부터 3개의 수를 키보드로 입력 받아 
+		    입력 받은 수가 모두 같으면 true, 아니면 false를 출력
+		*/
+	}
+
+}
