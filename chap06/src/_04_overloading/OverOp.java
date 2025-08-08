38270a52b1f099171ea520be4e1e681f22e5f93d
tjoeun42 <tjoeunit@gmail.com>
2025-08-08 21:45:29 +0900

    5일차_7교시

--- chap06/src/_04_overloading/OverOp.java
+++ chap06/src/_04_overloading/OverOp.java
@@ -1,5 +1,35 @@
 package _04_overloading;
 
 public class OverOp {
+	int num = 1;
 
+	int minus() {
+		return num;
+	}
+	int minus(int num1) {
+		return num1-num;
+	}
+	int minus(int num1, int num2) {
+		return num1-num2;
+	}
+	
+	int multiply() {
+		return num*2;
+	}
+	int multiply(int num1) {
+		return num*num1;
+	}
+	int multiply(int num1, int num2) {
+		return num1*num2;
+	}
+	
+	double divide() {
+		return num;
+	}
+	double divide(double num1) {
+		return num1/num;
+	}
+	double divide(double num1, double num2) {
+		return num1/num2;
+	}
 }
---
+++ chap06/src/_05_varArg/VarArg.java
@@ -0,0 +1,31 @@
+package _05_varArg;
+
+public class VarArg {
+	public static void main(String[] args) {
+		/*  메소드 앞에  static을 안붙이면 객체 생성 해야됨
+		VarArg v = new VarArg();
+		v.method("a", "b", "c");
+		*/
+		method("a", "b", "c");
+		method("a", "b", "c", "d", "e", "f");
+	}
+	/*
+	void method(String s1) {
+		System.out.println(s1);
+	}
+	
+	void method(String s1, String s2) {
+		System.out.println(s1 + " " + s2);
+	}
+	
+	void method(String s1, String s2, String s3) {
+		System.out.println(s1 + " " + s2 + " " + s3);
+	}
+	*/
+	static void method(String... str) {
+		for(String s : str) {
+			System.out.print(s + " ");
+		}
+		System.out.println();
+	}
+}
